df623e8dd17d6f632807295776d827e1703439e8
agatacieliczko <agata.cieliczko3@gmail.com>
2026-02-23 11:23:33 +0100

    Q-learning

--- Q_learning.py
+++ Q_learning.py
@@ -14,7 +14,12 @@ class QLearningAgent(BaseAgent):
         
     def update(self,s,a,r,s_next,done):
         # TO DO: Add own code
-        pass
+        if done:
+            target = r
+        else:
+            target = r + self.gamma * np.max(self.Q_sa[s_next])
+
+        self.Q_sa[s, a] += self.learning_rate * (target - self.Q_sa[s, a])
 
 def q_learning(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None, temp=None, plot=True, eval_interval=500):
     ''' runs a single repetition of q_learning
@@ -27,7 +32,35 @@ def q_learning(n_timesteps, learning_rate, gamma, policy='egreedy', epsilon=None
     eval_returns = []
     
     # TO DO: Write your Q-learning algorithm here!
-    
+    # sample initial state
+    s = env.reset()
+
+    # while budget do
+    for t in range(n_timesteps):
+
+        # sample action
+        a = agent.select_action(s, policy=policy, epsilon=epsilon, temp=temp)
+
+        # simulate environment
+        step_out = env.step(a)
+        if len(step_out) == 3:
+            s_next, r, done = step_out
+        else:
+            s_next, r, done, _ = step_out
+
+        # Q update
+        agent.update(s, a, r, s_next, done)
+
+        # reset environment if episode ended
+        if done:
+            s = env.reset()
+        else:
+            s = s_next
+
+        # evaluate
+        if (t + 1) % eval_interval == 0:
+            eval_returns.append(agent.evaluate(eval_env))
+            eval_timesteps.append(t + 1)
     # if plot:
     #    env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during Q-learning execution
 
