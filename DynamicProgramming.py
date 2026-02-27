2bd6f2246d872a4a08cddd3e0873516df1eb187f
Your Name <your@email.com>
2026-02-27 14:21:49 +0100

    Dynamic Progamming

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -22,14 +22,17 @@ class QValueIterationAgent:
     def select_action(self,s):
         ''' Returns the greedy best action in state s ''' 
         # TO DO: Add own code
-        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
-        return a
+        return argmax(self.Q_sa[s,:])
         
     def update(self,s,a,p_sas,r_sas):
         ''' Function updates Q(s,a) using p_sas and r_sas '''
         # TO DO: Add own code
-        pass
-    
+        old_q = self.Q_sa[s, a]
+        v_next = np.max(self.Q_sa, axis=1)  # v(s') = max_a' Q(s',a') for all s'
+        new_q = np.sum(p_sas * (r_sas + self.gamma * v_next))
+        self.Q_sa[s, a] = new_q
+        return abs(new_q - old_q)
+            
     
 def Q_value_iteration(env, gamma=1.0, threshold=0.001):
     ''' Runs Q-value iteration. Returns a converged QValueIterationAgent object '''
@@ -41,6 +44,21 @@ def Q_value_iteration(env, gamma=1.0, threshold=0.001):
     # Plot current Q-value estimates & print max error
     # env.render(Q_sa=QIagent.Q_sa,plot_optimal_policy=True,step_pause=0.2)
     # print("Q-value iteration, iteration {}, max error {}".format(i,max_error))
+
+    # Q-value iteration (Alg. 1): repeatedly sweep through all (s,a) and update
+    i = 0
+    max_error = np.inf
+    while max_error > threshold:
+        max_error = 0.0
+        for s in range(env.n_states):
+            for a in range(env.n_actions):
+                p_sas, r_sas = env.model(s, a)
+                err = QIagent.update(s, a, p_sas, r_sas)
+                if err > max_error:
+                    max_error = err
+        env.render(Q_sa=QIagent.Q_sa, plot_optimal_policy=True, step_pause=0.2)
+        print("Q-value iteration, iteration {}, max error {}".format(i, max_error))
+        i += 1
  
     return QIagent
 
@@ -62,6 +80,29 @@ def experiment():
 
     # TO DO: Compute mean reward per timestep under the optimal policy
     # print("Mean reward per timestep under optimal policy: {}".format(mean_reward_per_timestep))
+
+    # Compute mean reward per timestep under the optimal policy
+
+    n_episodes = 100
+    total_reward = 0
+    total_timesteps = 0
+
+    for _ in range(n_episodes):
+        s = env.reset()
+        done = False
+        
+        while not done:
+            a = QIagent.select_action(s)   # greedy action
+            s_next, r, done = env.step(a)
+            
+            total_reward += r
+            total_timesteps += 1
+            
+            s = s_next
+
+    mean_reward_per_timestep = total_reward / total_timesteps
+
+    print("Mean reward per timestep under optimal policy: {}".format(mean_reward_per_timestep))
     
 if __name__ == '__main__':
     experiment()
